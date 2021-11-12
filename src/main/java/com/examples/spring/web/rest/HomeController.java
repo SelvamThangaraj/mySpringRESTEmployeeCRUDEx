a544274bae8456e3aeabd39786eab42d7cfaa05b
ST <44888490+SelvamThangaraj@users.noreply.github.com>
2021-11-12 11:19:10 +0530

    Update HomeController.java

--- src/main/java/com/examples/spring/web/rest/HomeController.java
+++ src/main/java/com/examples/spring/web/rest/HomeController.java
@@ -1,16 +1,21 @@
 package com.examples.spring.web.rest;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+	@Autowired
+	Environment env;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
-		return "Welcome to Employee Management Service!";
+		return "Welcome to Employee Management Service! Developer=>"+env.getProperty("developer")
+		+" Broswer=>"+env.getProperty("browser");
 	}
 	
 }
